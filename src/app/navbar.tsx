83f2f25afa3f973fc0483e28918ae1484bf02b38
CPGRU <pei19860202@gmail.com>
2024-09-10 11:52:39 +0200

    add navbar

---
+++ src/app/navbar.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ArrowRightEndOnRectangleIcon } from "@heroicons/react/16/solid";
+
+export default function NavBar(){
+    return (
+        <div className="container">
+            <div className="flex items-center justify-between h-32">
+                <div className="ml-10">
+                    <Link href="/">
+                        <Image src='/logo.png' alt='logo' width={200} height={200} />
+                    </Link>
+                </div>
+                
+                <div>
+                    <Link href='/login' className="flex flex-row items-center">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="size-5">
+                            <path fill-rule="evenodd" d="M17 4.25A2.25 2.25 0 0 0 14.75 2h-5.5A2.25 2.25 0 0 0 7 4.25v2a.75.75 0 0 0 1.5 0v-2a.75.75 0 0 1 .75-.75h5.5a.75.75 0 0 1 .75.75v11.5a.75.75 0 0 1-.75.75h-5.5a.75.75 0 0 1-.75-.75v-2a.75.75 0 0 0-1.5 0v2A2.25 2.25 0 0 0 9.25 18h5.5A2.25 2.25 0 0 0 17 15.75V4.25Z" clip-rule="evenodd" />
+                            <path fill-rule="evenodd" d="M1 10a.75.75 0 0 1 .75-.75h9.546l-1.048-.943a.75.75 0 1 1 1.004-1.114l2.5 2.25a.75.75 0 0 1 0 1.114l-2.5 2.25a.75.75 0 1 1-1.004-1.114l1.048-.943H1.75A.75.75 0 0 1 1 10Z" clip-rule="evenodd" />
+                        </svg>
+                        Login
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
